8319020692bc4972d04fd6fd8ca1530e1b6d5959
Daniel Tapia <dan.tg@outlook.com>
2015-03-27 21:55:48 -0400

    Added user labels to comment bubbles to distinguish sellers and staff.

--- Brocfile.js
+++ Brocfile.js
@@ -18,7 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-app.import('bower_components/bootstrap/dist/css/bootstrap-theme.min.css');
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
 
